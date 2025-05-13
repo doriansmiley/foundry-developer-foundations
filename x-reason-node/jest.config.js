44cd49e7263ee8968de1d408923988e11784b82a
doriansmiley <doriansmiley@siliconpublishing.com>
2025-05-12 22:02:02 -0700

    fix jest setup

---
+++ x-reason-node/jest.config.js
@@ -0,0 +1,39 @@
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+
+    // Treat JS and TS as ESM
+    extensionsToTreatAsEsm: ['.ts'],
+
+    testPathIgnorePatterns: ['<rootDir>/dist/'],
+
+    globals: {
+        'ts-jest': {
+            tsconfig: 'tsconfig.json',
+            useESM: true,
+        },
+    },
+
+    testMatch: [
+        "**/test/**/*.test.ts",
+        "**/test/**/*.test.js"
+    ],
+
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+        '^.+\\.js$': 'ts-jest',
+    },
+
+    // Allow ts-jest to transform @osdk/* ESM modules
+    transformIgnorePatterns: [
+        'node_modules/(?!(?:@osdk)/)',
+    ],
+
+    moduleNameMapper: {
+        // Strip `.js` from your TS imports so ESM paths resolve
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+        '^@xreason/(.*)$': '<rootDir>/src/$1',
+        '^@osdk/shared\\.client$': '<rootDir>/node_modules/@osdk/shared.client/index.js',
+        '^@osdk/shared\\.client2$': '<rootDir>/node_modules/@osdk/shared.client2/index.js',
+    },
+};
