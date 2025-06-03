be1d3d93b070b66910e4e63f603db796f3295232
doriansmiley <doriansmiley@siliconpublishing.com>
2025-06-03 09:27:01 -0700

    update script to ignore file

--- .github/actions/test/getChangedPaths.js
+++ .github/actions/test/getChangedPaths.js
@@ -18,7 +18,7 @@ async function getChangedPaths(baseRef = 'origin/master') {
 
         compare.files.forEach(file => {
             const dir = file.filename.split('/')[0];
-            if (dir && dir !== '.github') directories.add(dir);
+            if (dir && dir !== '.github' && dir !== '.gitignore') directories.add(dir);
         });
 
         return Array.from(directories);
