57dabf4370103acc304ec77bd972f594fde69e23
Przemyslaw Nowak <nowak_przemek@yahoo.com>
2025-08-02 14:25:46 +0200

    rename data-acces-x-reason into data-access-platform

--- packages/data-access/platform/jest.config.ts
+++ packages/data-access/platform/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'data-access-x-reason',
+  displayName: 'data-access-platform',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/packages/data-access/x-reason',
+  coverageDirectory: '../../../coverage/packages/data-access/platform',
 };
